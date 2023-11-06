f816628e3d36b6747fe94abab21a5e21816d4306
aysegulbilici <103766407+aysegulbilici@users.noreply.github.com>
2023-11-06 12:42:12 +0300

    Proje dosyası ekle.

---
+++ pratik/Controllers/CourseController.cs
@@ -0,0 +1,35 @@
+﻿using pratik.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace pratik.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            //kurs nesnesi tanımladık
+            var kurs = new Course();
+
+            kurs.Id = 1;
+            kurs.Title = "Aspnet core kursu";
+            kurs.Description = "Güzel bir kurs";
+            kurs.Image = "kedi.jpeg";
+            //kurs objesini model olarak viewa gönderdik
+            return View(kurs);
+        }
+
+        public IActionResult List()
+        {
+            //listenin tipi Course olan liste oluşturduk.
+            var kurslar = new List<Course>()
+            {
+                //Listeye elemanlar ekledik.
+                new Course(){Id=1, Title="aspnet kursu", Description="güzelbir kurs", Image="kedi.jpeg"},
+                new Course(){Id=2, Title="php kursu", Description="güzelbir kurs",Image="kopek.jpeg"},
+                new Course(){Id=3, Title="django kursu", Description="güzelbir kurs",Image="panda.jpeg"}
+            };
+            return View("CourseList", kurslar);
+        }
+    }
+}
---
+++ pratik/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pratik.Models;
+using System.Diagnostics;
+
+namespace pratik.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
